c8f466551eecbc53919a22cac043fc642938237a
Rohan1279 <muniruddinrohan@gmail.com>
2022-11-25 22:56:10 +0600

    admin route api done

--- index.js
+++ index.js
@@ -83,6 +83,13 @@ async function run() {
       const user = await usersCollection.findOne(query);
       res.send({ isSeller: user?.userRole === "Seller" });
     });
+    // find if the is an admin
+    app.get("/users/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const user = await usersCollection.findOne(query);
+      res.send({ isAdmin: user?.userRole === "Admin" });
+    });
     app.get("/categories", async (req, res) => {
       const query = {};
       const categories = await categoriesCollection.find(query).toArray();
